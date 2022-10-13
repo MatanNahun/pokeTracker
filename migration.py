b94b6ddc9921f4102e295a69507fd0c21cf7926f
MatanNahum <matan.nahum95@gmail.com>
2022-10-13 15:43:25 +0300

    first initial project

---
+++ migration.py
@@ -0,0 +1,10 @@
+import json
+
+
+with open("pokemonDBJson.json") as file:
+    data = json.load(file)
+
+
+def migration():
+
+    return
---
+++ pokemon.sql
@@ -0,0 +1,32 @@
+-- CREATE DATABASE pokeTracker;
+
+USE pokeTracker;
+
+-- CREATE TABLE pokemons(
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255),
+--     type VARCHAR(255),
+--     height INT,
+--     weight INT
+-- );
+
+
+
+
+-- CREATE TABLE trainers(
+--    
+--     name VARCHAR(255),
+--     town VARCHAR(255),
+--     PRIMARY KEY (name)
+-- );
+
+
+-- CREATE TABLE pokemons_trainers(
+--     id_pokemon INT,
+--     trainer VARCHAR(255),
+
+--     PRIMARY KEY (id_pokemon, trainer),
+
+--     FOREIGN KEY(id_pokemon) REFERENCES pokemons(id),
+--     FOREIGN KEY(id_trainer) REFERENCES trainers(name)
+-- );
