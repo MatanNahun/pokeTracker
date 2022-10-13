d6b02e0651a10488a17e909ceb4108d6375500a1
eladshachar <elad.shachar@gmail.com>
2022-10-13 16:58:48 +0300

    added fill_trainers function

--- pokemon.sql
+++ pokemon.sql
@@ -2,31 +2,31 @@
 
 USE pokeTracker;
 
--- CREATE TABLE pokemons(
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(255),
---     type VARCHAR(255),
---     height INT,
---     weight INT
--- );
+CREATE TABLE pokemons(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    height INT,
+    weight INT
+);
 
 
 
 
--- CREATE TABLE trainers(
---    
---     name VARCHAR(255),
---     town VARCHAR(255),
---     PRIMARY KEY (name)
--- );
+CREATE TABLE trainers(
+   
+    name VARCHAR(255),
+    town VARCHAR(255),
+    PRIMARY KEY (name)
+);
 
 
--- CREATE TABLE pokemons_trainers(
---     id_pokemon INT,
---     trainer VARCHAR(255),
+CREATE TABLE pokemons_trainers(
+    id_pokemon INT,
+    trainer VARCHAR(255),
 
---     PRIMARY KEY (id_pokemon, trainer),
+    PRIMARY KEY (id_pokemon, trainer),
 
---     FOREIGN KEY(id_pokemon) REFERENCES pokemons(id),
---     FOREIGN KEY(id_trainer) REFERENCES trainers(name)
--- );
+    FOREIGN KEY(id_pokemon) REFERENCES pokemons(id),
+    FOREIGN KEY(trainer) REFERENCES trainers(name)
+);
