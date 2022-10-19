f098af5f686f30fe0115409724093fe04662d25e
MatanNahum <matan.nahum95@gmail.com>
2022-10-19 14:54:00 +0300

    get pokemon id

--- tests.py
+++ tests.py
@@ -1,1 +1,8 @@
+from http import client, server
 import pytest
+from unittest.mock import patch as mock_patch
+from fastapi.testclient import TestClient
+
+from server import app
+
+client = TestClient(app)
