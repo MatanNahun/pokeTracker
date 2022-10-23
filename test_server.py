6f7863bd5bdfce2626d23dfb2738d2e836c58782
MatanNahum <matan.nahum95@gmail.com>
2022-10-23 17:22:29 +0300

    added get_pokemon_by_type test

--- test_server.py
+++ test_server.py
@@ -17,3 +17,10 @@ def test_get_trainers_correct_poke_name():
 def test_get_trainers_incorrect_poke_name():
     response = client.get("/trainers?pokemon=ditoooor")
     assert response.status_code == 400
+
+
+def test_get_pokemons_by_type():
+    response = client.get("http://127.0.0.1:8000/pokemons?type=normal")
+    assert "eevee" in response.json()
+    assert "charizard" not in response.json()
+    assert response.status_code == 201
